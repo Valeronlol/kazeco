8bd1bfb72d30067995fa3ecdfd2e2850040a105f
Valeron <green-travel.kg@mail.ru>
2016-09-18 14:30:34 +0600

    styles added

---
+++ media/app/js/common.js
@@ -0,0 +1,55 @@
+$( document ).ready(function() {
+
+    $('#slider_top').slick({
+        dots: true,
+        infinite: true,
+        speed: 800,
+        fade: true,
+        cssEase: 'linear',
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+    $('#middle_slider').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    //middle slider hover effect
+    $('#middle_slider .slick-slide').on('mouseenter', function () {
+        $(this).children('.hidden_hover').fadeIn(500);
+    });
+    $('#middle_slider .slick-slide').on('mouseleave', function () {
+        $(this).children('.hidden_hover').fadeOut(500);
+    })
+
+
+});
