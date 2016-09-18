4d3ce6a8b805ed8fbc67523c16d8e08b7d6c43af
Valeron <green-travel.kg@mail.ru>
2016-09-18 13:41:31 +0600

    added changes (reverted from commit 7a1dcd0fef30ddef199ad99d1b7122769a9756b1)

--- config.php
+++ config.php
@@ -22,7 +22,7 @@ define('DIR_UPLOAD', 'C:/OpenServer/domains/shop.local/system/storage/upload/');
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '123');
 define('DB_DATABASE', 'shop.local');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
