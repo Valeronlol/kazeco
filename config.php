ed8a21c57062f9311e1e3a5dc00fe90b406b9129
dss-1210 <profile_123@mail.ru>
2016-09-18 13:07:18 +0600

    added changes

--- config.php
+++ config.php
@@ -22,8 +22,8 @@ define('DIR_UPLOAD', 'C:/OpenServer/domains/shop.local/system/storage/upload/');
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '1');
-define('DB_DATABASE', 'kazeco.local');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shop.local');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 
