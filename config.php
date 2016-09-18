0d2398efef1ba178d473c5d210f5dea948a6ea2f
dss-1210 <profile_123@mail.ru>
2016-09-18 13:29:47 +0600

    added changes 5

--- config.php
+++ config.php
@@ -22,8 +22,8 @@ define('DIR_UPLOAD', 'C:/OpenServer/domains/shop.local/system/storage/upload/');
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'sho1.local');
+define('DB_PASSWORD', '123');
+define('DB_DATABASE', 'shop.local');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 
