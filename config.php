7e805514373a0b229366912397f7c3d464924fc2
Valeron <green-travel.kg@mail.ru>
2016-09-18 12:57:07 +0600

    Add project

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shop.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shop.local/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/shop.local/catalog/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/shop.local/system/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/shop.local/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/shop.local/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/shop.local/system/config/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/shop.local/image/');
+define('DIR_CACHE', 'C:/OpenServer/domains/shop.local/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/shop.local/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/shop.local/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/shop.local/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/shop.local/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '1');
+define('DB_DATABASE', 'kazeco.local');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
