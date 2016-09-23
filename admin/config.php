37ed64597ca885fde13b846591e0a1b6a2f0ad21
Valeron <green-travel.kg@mail.ru>
2016-09-23 22:39:33 +0600

    added from nout

--- admin/config.php
+++ admin/config.php
@@ -1,31 +1,31 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://shop.local/admin/');
-define('HTTP_CATALOG', 'http://shop.local/');
+define('HTTP_SERVER', 'http://kazeco.local/admin/');
+define('HTTP_CATALOG', 'http://kazeco.local/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://shop.local/admin/');
-define('HTTPS_CATALOG', 'http://shop.local/');
+define('HTTPS_SERVER', 'http://kazeco.local/admin/');
+define('HTTPS_CATALOG', 'http://kazeco.local/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/OpenServer/domains/shop.local/admin/');
-define('DIR_SYSTEM', 'C:/OpenServer/domains/shop.local/system/');
-define('DIR_LANGUAGE', 'C:/OpenServer/domains/shop.local/admin/language/');
-define('DIR_TEMPLATE', 'C:/OpenServer/domains/shop.local/admin/view/template/');
-define('DIR_CONFIG', 'C:/OpenServer/domains/shop.local/system/config/');
-define('DIR_IMAGE', 'C:/OpenServer/domains/shop.local/image/');
-define('DIR_CACHE', 'C:/OpenServer/domains/shop.local/system/storage/cache/');
-define('DIR_DOWNLOAD', 'C:/OpenServer/domains/shop.local/system/storage/download/');
-define('DIR_LOGS', 'C:/OpenServer/domains/shop.local/system/storage/logs/');
-define('DIR_MODIFICATION', 'C:/OpenServer/domains/shop.local/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/OpenServer/domains/shop.local/system/storage/upload/');
-define('DIR_CATALOG', 'C:/OpenServer/domains/shop.local/catalog/');
+define('DIR_APPLICATION', 'C:/openserver/OpenServer/domains/kazeco.local/admin/');
+define('DIR_SYSTEM', 'C:/openserver/OpenServer/domains/kazeco.local/system/');
+define('DIR_LANGUAGE', 'C:/openserver/OpenServer/domains/kazeco.local/admin/language/');
+define('DIR_TEMPLATE', 'C:/openserver/OpenServer/domains/kazeco.local/admin/view/template/');
+define('DIR_CONFIG', 'C:/openserver/OpenServer/domains/kazeco.local/system/config/');
+define('DIR_IMAGE', 'C:/openserver/OpenServer/domains/kazeco.local/image/');
+define('DIR_CACHE', 'C:/openserver/OpenServer/domains/kazeco.local/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/openserver/OpenServer/domains/kazeco.local/system/storage/download/');
+define('DIR_LOGS', 'C:/openserver/OpenServer/domains/kazeco.local/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/openserver/OpenServer/domains/kazeco.local/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/openserver/OpenServer/domains/kazeco.local/system/storage/upload/');
+define('DIR_CATALOG', 'C:/openserver/OpenServer/domains/kazeco.local/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'shop.local');
+define('DB_DATABASE', 'kazeco.local');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
