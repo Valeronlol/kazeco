001e869a85d5d4f6343d5b06e0be38c871b19a18
dss-1210 <profile_123@mail.ru>
2016-09-25 16:29:30 +0600

    Добавлены категории на главную. Переменная $categories в виде.

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -16,6 +16,58 @@ class ControllerCommonHome extends Controller {
 		$data['footer'] = $this->load->controller('common/footer');
 		$data['header'] = $this->load->controller('common/header');
 
+		/* Categories */
+		$this->load->model('catalog/category');
+		$this->load->model('tool/image');
+
+		$data['categories'] = array();
+
+		$categories = $this->model_catalog_category->getCategories(0);
+
+		foreach ($categories as $category) {
+			if ($category['top']) {
+				// Level 2
+				$children_data = array();
+
+				$children = $this->model_catalog_category->getCategories($category['category_id']);
+
+				foreach ($children as $child) {
+					$filter_data = array(
+						'filter_category_id'  => $child['category_id'],
+						'filter_sub_category' => true
+					);
+
+					$children_data[] = array(
+						'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+						'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id']),
+						'image' => $child['image'],
+						'thumb' => ''
+					);
+
+					if($child['image'] && is_file(DIR_IMAGE . $child['image'])){
+						$children_data['thumb'] = $this->model_tool_image->resize($child['image'], 100, 100);
+					}
+				}
+
+				$category_array = array(
+					'name'     => $category['name'],
+					'children' => $children_data,
+					'column'   => $category['column'] ? $category['column'] : 1,
+					'href'     => $this->url->link('product/category', 'path=' . $category['category_id']),
+					'image'    => $category['image'],
+					'thumb'    => ''
+				);
+
+				if($category_array['image'] && is_file(DIR_IMAGE . $category_array['image'])){
+					$category_array['thumb'] = $this->model_tool_image->resize($category_array['image'], 100, 100);
+				}
+
+				// Level 1
+				$data['categories'][] = $category_array;
+			}
+		}
+		/* Categories */
+
 		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/home.tpl')) {
 			$this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/common/home.tpl', $data));
 		} else {
